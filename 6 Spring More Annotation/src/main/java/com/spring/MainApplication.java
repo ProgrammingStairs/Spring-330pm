947520c95579294f69430c06c5b3828dc5e6442f
gauravKothari <kotharigaurav6@gmail.com>
2025-11-08 16:54:59 +0530

    spring

--- 6 Spring More Annotation/src/main/java/com/spring/MainApplication.java
+++ 6 Spring More Annotation/src/main/java/com/spring/MainApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.stereotype.Component;
 
 import com.spring.service.EmailService;
@@ -45,5 +46,14 @@ public class MainApplication{
 		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		MainApplication app = context.getBean(MainApplication.class);
 		app.actionedPerformed();
+		
+		SMSService sms1 = context.getBean(SMSService.class);
+		System.out.println("sms1 : "+sms1);
+		sms1.printSmsService("Sms First");
+		SMSService sms2 = context.getBean(SMSService.class);
+		System.out.println("sms2 : "+sms2);
+		sms2.printSmsService("Sms Second");	
+		
+		((AbstractApplicationContext) context).close(); // it runs @PreDestroy annoted method on @Scope("singleton")
 	}
 }
--- 6 Spring More Annotation/src/main/java/com/spring/service/SMSService.java
+++ 6 Spring More Annotation/src/main/java/com/spring/service/SMSService.java
@@ -1,9 +1,15 @@
 package com.spring.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Service
+@Scope("singleton")
+//@Scope("prototype")
 public class SMSService{
 	@Value("${app.number}")
 	String number;
@@ -14,5 +20,19 @@ public class SMSService{
 	
 	public void printSmsService(String message) {
 		System.out.println("You received text sms : "+message+" from this sender : "+number);
+	}
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("sms service is initialized");
+	}
+	/*
+	Note:
+	here in above annotation case, @PostConstruct method runs when bean is initialized and all dependencies injections are injected.
+	*/
+	
+	@PreDestroy
+	public void destroy() {
+		System.out.println("sms service is destroyed");
 	}
 }
